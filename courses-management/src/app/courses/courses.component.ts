18397e7b29cfce6752d37c961fb3cecf07535a9b
HuidanKuang <77803403+HuidanKuang@users.noreply.github.com>
2022-06-08 20:08:13 -0400

    Create component

---
+++ courses-management/src/app/courses/courses.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+
+  course:Course = {
+    id:1,
+    name:'MCV4U'
+  };
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
