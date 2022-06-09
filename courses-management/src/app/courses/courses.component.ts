b73989d08979bf8c9e2c8a2c7df6f8a25215d443
HuidanKuang <77803403+HuidanKuang@users.noreply.github.com>
2022-06-08 20:46:10 -0400

    display the records on screen

--- courses-management/src/app/courses/courses.component.ts
+++ courses-management/src/app/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../course';
+import {COURSES} from '../mock-courses';
 
 @Component({
   selector: 'app-courses',
@@ -13,9 +14,17 @@ export class CoursesComponent implements OnInit {
     name:'MCV4U'
   };
   
+  courses = COURSES;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectedCourse?: Course;
+  onSelect(course: Course): void {
+    this.selectedCourse = course;
+  }
+
 }
+
---
+++ courses-management/src/app/mock-courses.ts
@@ -0,0 +1,14 @@
+import { Course } from "./course";
+
+export const COURSES : Course[] = [
+    {id: 1, name: 'MCV4U'},
+    {id: 2, name: 'MDM4U'},
+    {id: 3, name: 'BBI1O'},
+    {id: 4, name: 'BTT10'},
+    {id: 5, name: 'CLN4U'},
+    {id: 6, name: 'CLN4C'},
+    {id: 7, name: 'ENG3U'},
+    {id: 8, name: 'ENG4U'},
+    {id: 9, name: 'ICS2O'},
+    {id: 10, name: 'ICS3C'},
+];
